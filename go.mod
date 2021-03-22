05a3cf7a4290114fe9fa4bcd51e23d241bd23b9b
Alex Ashley <alexa@liatrio.com>
2021-03-22 14:56:33 -0500

    Initial setup

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/rode/collector-build
+
+go 1.15
+
+require (
+	github.com/onsi/gomega v1.11.0
+	github.com/rode/rode v0.1.1 // indirect
+	google.golang.org/grpc v1.36.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,69 @@
+// Copyright 2021 The Rode Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"context"
+	"crypto/tls"
+	"log"
+	"os"
+	"time"
+
+	pb "github.com/rode/rode/proto/v1alpha1"
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+
+	"github.com/rode/collector-build/config"
+)
+
+func main()  {
+	conf, err := config.Build(os.Args[0], os.Args[1:])
+	if err != nil {
+		log.Fatalf("error parsing flags: %v", err)
+	}
+
+	logger, err := createLogger(conf.Debug)
+	if err != nil {
+		log.Fatalf("failed to create logger: %v", err)
+	}
+
+	dialOptions := []grpc.DialOption{
+		grpc.WithBlock(),
+	}
+	if conf.RodeConfig.Insecure {
+		dialOptions = append(dialOptions, grpc.WithInsecure())
+	} else {
+		dialOptions = append(dialOptions, grpc.WithTransportCredentials(credentials.NewTLS(&tls.Config{})))
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+	conn, err := grpc.DialContext(ctx, conf.RodeConfig.Host, dialOptions...)
+	if err != nil {
+		logger.Fatal("failed to establish grpc connection to Rode", zap.Error(err))
+	}
+	defer conn.Close()
+
+	_ = pb.NewRodeClient(conn)
+}
+
+func createLogger(debug bool) (*zap.Logger, error) {
+	if debug {
+		return zap.NewDevelopment()
+	}
+
+	return zap.NewProduction()
+}
