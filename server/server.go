d1d411dd4629d3207379cfaf16a96241ae8b042f
Brad <bradf@liatrio.com>
2021-03-23 11:02:54 -0700

    Add commit ID field and add error checking

--- server/server.go
+++ server/server.go
@@ -16,6 +16,8 @@ package server
 
 import (
 	"context"
+	"fmt"
+	"net/url"
 
 	"github.com/golang/protobuf/ptypes"
 	"github.com/google/uuid"
@@ -27,6 +29,8 @@ import (
 	"github.com/rode/rode/protodeps/grafeas/proto/v1beta1/provenance_go_proto"
 	"github.com/rode/rode/protodeps/grafeas/proto/v1beta1/source_go_proto"
 	"go.uber.org/zap"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 const (
@@ -47,6 +51,18 @@ func NewBuildCollectorServer(logger *zap.Logger, rode pb.RodeClient) *BuildColle
 }
 
 func (s *BuildCollectorServer) CreateBuild(ctx context.Context, request *v1alpha1.CreateBuildRequest) (*v1alpha1.CreateBuildResponse, error) {
+	if len(request.Repository) == 0 {
+		return nil, status.Error(codes.InvalidArgument, "Request had no repository specified")
+	}
+
+	if len(request.Artifacts) == 0 {
+		return nil, status.Error(codes.InvalidArgument, "Request had no artifacts specified")
+	}
+
+	if len(request.CommitId) == 0 {
+		return nil, status.Error(codes.InvalidArgument, "Request had no commit ID specified")
+	}
+
 	var artifacts []*provenance_go_proto.Artifact
 
 	for _, artifact := range request.Artifacts {
@@ -55,11 +71,16 @@ func (s *BuildCollectorServer) CreateBuild(ctx context.Context, request *v1alpha
 		})
 	}
 
+	repositoryURL, err := url.ParseRequestURI(request.Repository)
+	if err != nil {
+		return nil, status.Errorf(codes.InvalidArgument, "Invalid Repository URL: %s", err)
+	}
+
 	buildOccurrences := &pb.BatchCreateOccurrencesRequest{
 		Occurrences: []*grafeas_go_proto.Occurrence{
 			{
 				Resource: &grafeas_go_proto.Resource{
-					Uri: request.Repository,
+					Uri: fmt.Sprintf("git://%s%s@%s", repositoryURL.Host, repositoryURL.Path, request.CommitId),
 				},
 				NoteName: buildCollectorNote,
 				Kind:     common_go_proto.NoteKind_BUILD,
@@ -74,7 +95,8 @@ func (s *BuildCollectorServer) CreateBuild(ctx context.Context, request *v1alpha
 								Context: &source_go_proto.SourceContext{
 									Context: &source_go_proto.SourceContext_Git{
 										Git: &source_go_proto.GitSourceContext{
-											Url: request.Repository,
+											Url:        request.Repository,
+											RevisionId: request.CommitId,
 										}},
 								},
 							},
@@ -87,9 +109,11 @@ func (s *BuildCollectorServer) CreateBuild(ctx context.Context, request *v1alpha
 
 	response, err := s.rode.BatchCreateOccurrences(ctx, buildOccurrences)
 	if err != nil {
-		return nil, err
+		return nil, status.Errorf(codes.Internal, "Error creating occurrences in Rode: %s", err)
 	}
-
+	if len(response.Occurrences) != 1 {
+		return nil, status.Error(codes.FailedPrecondition, "Expected single occurrence to be returned")
+	}
 	return &v1alpha1.CreateBuildResponse{
 		BuildOccurrenceId: response.Occurrences[0].Name,
 	}, nil
