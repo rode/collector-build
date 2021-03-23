b6a1ed7fcf83a3c86a44e86d916485c4d8a28a7c
Alex Ashley <alexa@liatrio.com>
2021-03-23 11:54:13 -0500

    wip: create build occurrence

--- server/server.go
+++ server/server.go
@@ -17,9 +17,21 @@ package server
 import (
 	"context"
 
+	"github.com/golang/protobuf/ptypes"
+	"github.com/google/uuid"
 	"github.com/rode/collector-build/proto/v1alpha1"
 	pb "github.com/rode/rode/proto/v1alpha1"
+	"github.com/rode/rode/protodeps/grafeas/proto/v1beta1/build_go_proto"
+	"github.com/rode/rode/protodeps/grafeas/proto/v1beta1/common_go_proto"
+	"github.com/rode/rode/protodeps/grafeas/proto/v1beta1/grafeas_go_proto"
+	"github.com/rode/rode/protodeps/grafeas/proto/v1beta1/provenance_go_proto"
+	"github.com/rode/rode/protodeps/grafeas/proto/v1beta1/source_go_proto"
 	"go.uber.org/zap"
+)
+
+const (
+	rodeProjectId      = "projects/rode"
+	buildCollectorNote = rodeProjectId + "/notes/build_collector"
 )
 
 type BuildCollectorServer struct {
@@ -35,5 +47,50 @@ func NewBuildCollectorServer(logger *zap.Logger, rode pb.RodeClient) *BuildColle
 }
 
 func (s *BuildCollectorServer) CreateBuild(ctx context.Context, request *v1alpha1.CreateBuildRequest) (*v1alpha1.CreateBuildResponse, error) {
-	return &v1alpha1.CreateBuildResponse{}, nil
+	var artifacts []*provenance_go_proto.Artifact
+
+	for _, artifact := range request.Artifacts {
+		artifacts = append(artifacts, &provenance_go_proto.Artifact{
+			Id: artifact,
+		})
+	}
+
+	buildOccurrences := &pb.BatchCreateOccurrencesRequest{
+		Occurrences: []*grafeas_go_proto.Occurrence{
+			{
+				Resource: &grafeas_go_proto.Resource{
+					Uri: request.Repository,
+				},
+				NoteName: buildCollectorNote,
+				Kind:     common_go_proto.NoteKind_BUILD,
+				Details: &grafeas_go_proto.Occurrence_Build{
+					Build: &build_go_proto.Details{
+						Provenance: &provenance_go_proto.BuildProvenance{
+							Id:             uuid.New().String(),
+							ProjectId:      rodeProjectId,
+							BuiltArtifacts: artifacts,
+							CreateTime:     ptypes.TimestampNow(),
+							SourceProvenance: &provenance_go_proto.Source{
+								Context: &source_go_proto.SourceContext{
+									Context: &source_go_proto.SourceContext_Git{
+										Git: &source_go_proto.GitSourceContext{
+											Url: request.Repository,
+										}},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	response, err := s.rode.BatchCreateOccurrences(ctx, buildOccurrences)
+	if err != nil {
+		return nil, err
+	}
+
+	return &v1alpha1.CreateBuildResponse{
+		BuildOccurrenceId: response.Occurrences[0].Name,
+	}, nil
 }
